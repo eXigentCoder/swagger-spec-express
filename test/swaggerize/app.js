c36e336f71c6b6136625c4350c31096a047c877e
Ryan Kotzen <ryan@encentivize.co.za>
2016-08-28 15:48:21 +0200

    updating swaggerize tests

--- test/swaggerize/app.js
+++ test/swaggerize/app.js
@@ -6,10 +6,8 @@ describe('Swaggerize app tests', function () {
     beforeEach(function () {
         swagger.reset();
     });
-    it('Should work if the object is an express app', function () {
+    it('Should not throw an error if the object is an express app', function () {
         var app = express();
-        var router = new express.Router();
         swagger.swaggerize(app);
-        swagger.swaggerize(router);
     });
 });
--- test/swaggerize/router.js
+++ test/swaggerize/router.js
@@ -1,8 +1,13 @@
 'use strict';
 require('./../init');
 var swagger = require('../../lib/index');
+var express = require('express');
 describe('Spec', function () {
     beforeEach(function () {
         swagger.reset();
+    });
+    it('Should not throw an error if the object is an express app', function () {
+        var router = new express.Router();
+        swagger.swaggerize(router);
     });
 });
