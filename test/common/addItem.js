218370c6719bcd46cc7271d4601aeb8b609b981a
Ryan Kotzen <ryan@encentivize.co.za>
2016-08-28 14:33:59 +0200

    adding a test for if the model has definitions on it.

--- test/common/addItem.js
+++ test/common/addItem.js
@@ -22,6 +22,9 @@ function runCommonTestsForType(name, addFn, dataFn) {
         it('Should not throw an exception if validOptions are supplied', function () {
             expect(callAddFn(addFn, dataFn()), name).to.not.throw();
         });
+        it('Should throw an exception if you try add the same item again', function () {
+            expect(callAddFn(addFn, dataFn()), name).to.throw();
+        });
         it('Should throw an exception if no object is supplied', function () {
             expect(callAddFn(addFn, null), name).to.throw();
         });
@@ -38,6 +41,25 @@ function runCommonTestsForType(name, addFn, dataFn) {
             delete object.name;
             expect(callAddFn(addFn, object), name).to.throw();
         });
+        if (name === 'model') {
+            it('Should not throw an exception if the model has definitions in it', function () {
+                var model = dataFn();
+                model.name = model.name + "2";
+                model.definitions = {
+                    test: {
+                        type: "object",
+                        properties: {
+                            isTest: true
+                        },
+                        additionalProperties: false,
+                        required: [
+                            "isTest"
+                        ]
+                    }
+                };
+                expect(callAddFn(addFn, model), name).to.throw();
+            });
+        }
     });
 }
 
