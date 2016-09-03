55000ffb3365ab20180b0d919894360873722ce5
Ryan Kotzen <ryan@encentivize.co.za>
2016-09-03 20:56:39 +0200

    updating code so that validation initialisation happens as soon as the lib is included, required for ddescribing the routes before init is called

--- test/init.js
+++ test/init.js
@@ -1,15 +1,9 @@
 'use strict';
 var chai = require('chai');
 var dirtyChai = require('dirty-chai');
-var validator = require('../lib/validator');
-var State = require('../lib/state');
 chai.use(dirtyChai);
 
 global.chai = chai;
 global.expect = chai.expect;
 global.assert = chai.assert;
 global.should = chai.should();
-
-before(function () {
-    validator.initialise(State().ajvValidationOptions);
-});
