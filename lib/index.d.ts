5cb39c5462c15ffe359a35acf7573057ced24330
MrRefactoring <MrRefactoring@yandex.ru>
2019-05-18 11:50:52 +0300

    added basic types

---
+++ lib/index.d.ts
@@ -0,0 +1,66 @@
+import { Express } from 'express-serve-static-core';
+
+export interface IInitializeOptions {
+    document?: { [key: string]: any };
+    title?: string;
+    description?: string;
+    termsOfService?: string;
+    contact?: {
+        name?: string;
+        url?: string;
+        email?: string;
+    };
+    licence?: {
+        name?: string;
+        url?: string;
+    };
+    version?: string;
+    host?: string;
+    basePath?: string;
+    schemes?: string[];
+    consumes?: string[];
+    produces?: string[];
+    paths?: { [key: string]: any };
+    definitions?: { [key: string]: any };
+    parameters?: { [key: string]: any };
+    responses?: { [key: string]: any };
+    securityDefinitions?: { [key: string]: any };
+    security?: { [key: string]: any };
+    defaultSecurity?: string | string[];
+    tags?: {
+        name?: string;
+        description?: string;
+        externalDocs?: {
+            description?: string;
+            url: string;
+        }
+    };
+    externalDocs?: {
+        description?: string
+        url: string;
+    }
+}
+
+export namespace common {
+  function addModel(model: any, inputOptions: any): void;
+  function addResponse(response: any, options: any): void;
+  function addResponseHeader(responseHeader: any, options: any): void;
+  function addTag(tag: any, options: any): void;
+  namespace parameters {
+    function addBody(body: any, options: any): void;
+    function addFormData(formData: any, options: any): void;
+    function addHeader(header: any, options: any): void;
+    function addPath(path: any, options: any): void;
+    function addQuery(query: any, options: any): void;
+  }
+}
+export function compile(): void;
+
+export function initialise(app: Express, options: IInitializeOptions): void;
+export function initialize(app: Express, options: IInitializeOptions): void;
+
+export function json(): any;
+export function reset(): void;
+export function swaggerise(item: any): void;
+export function swaggerize(item: any): void;
+export function validate(): any;
