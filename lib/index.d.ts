2a76a4a7c9d3363970ff35d492d7ecb1156b4c25
MrRefactoring <MrRefactoring@yandex.ru>
2019-05-18 13:05:59 +0300

    options marked as optional argument

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -50,16 +50,16 @@ interface IValid {
 interface JsonObject { [key: string]: any; }
 
 export namespace common {
-  function addModel(model: any, inputOptions: any): void;
-  function addResponse(response: any, options: any): void;
-  function addResponseHeader(responseHeader: any, options: any): void;
-  function addTag(tag: any, options: any): void;
+  function addModel(model: any, inputOptions?: any): void;
+  function addResponse(response: any, options?: any): void;
+  function addResponseHeader(responseHeader: any, options?: any): void;
+  function addTag(tag: any, options?: any): void;
   namespace parameters {
-    function addBody(body: any, options: any): void;
-    function addFormData(formData: any, options: any): void;
-    function addHeader(header: any, options: any): void;
-    function addPath(path: any, options: any): void;
-    function addQuery(query: any, options: any): void;
+    function addBody(body: any, options?: any): void;
+    function addFormData(formData: any, options?: any): void;
+    function addHeader(header: any, options?: any): void;
+    function addPath(path: any, options?: any): void;
+    function addQuery(query: any, options?: any): void;
   }
 }
 export function compile(): void;
